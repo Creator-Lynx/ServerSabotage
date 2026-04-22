b039156141799d4a5c661067e19fcb8e2b4c845d
CreatorLynx <1_linx_1@mail.ru>
2026-04-22 09:13:54 +0300

    tested loading with progres function

--- ServerSabotage-unity/Assets/GameSystems&Objects/GameTerminal/TerminalManage.cs
+++ ServerSabotage-unity/Assets/GameSystems&Objects/GameTerminal/TerminalManage.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TerminalManage : MonoBehaviour
 {
@@ -23,6 +24,15 @@ public class TerminalManage : MonoBehaviour
 
     void Update()
     {
-        
+        if(Keyboard.current.digit1Key.wasPressedThisFrame) writer.SetLoadingProgress(10);
+        if(Keyboard.current.digit2Key.wasPressedThisFrame) writer.SetLoadingProgress(20);
+        if(Keyboard.current.digit3Key.wasPressedThisFrame) writer.SetLoadingProgress(30);
+        if(Keyboard.current.digit4Key.wasPressedThisFrame) writer.SetLoadingProgress(40);
+        if(Keyboard.current.digit5Key.wasPressedThisFrame) writer.SetLoadingProgress(50);
+        if(Keyboard.current.digit6Key.wasPressedThisFrame) writer.SetLoadingProgress(60);
+        if(Keyboard.current.digit7Key.wasPressedThisFrame) writer.SetLoadingProgress(70);
+        if(Keyboard.current.digit8Key.wasPressedThisFrame) writer.SetLoadingProgress(80);
+        if(Keyboard.current.digit9Key.wasPressedThisFrame) writer.SetLoadingProgress(90);
+        if(Keyboard.current.digit0Key.wasPressedThisFrame) writer.SetLoadingProgress(100);
     }
 }
