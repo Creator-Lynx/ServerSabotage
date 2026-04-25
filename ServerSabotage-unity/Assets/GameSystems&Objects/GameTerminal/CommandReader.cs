0b807bd6307a98d2f0d8fe1e3087e4706b9044b9
CreatorLynx <1_linx_1@mail.ru>
2026-04-25 09:38:06 +0300

    unstable command reader

--- ServerSabotage-unity/Assets/GameSystems&Objects/GameTerminal/CommandReader.cs
+++ ServerSabotage-unity/Assets/GameSystems&Objects/GameTerminal/CommandReader.cs
@@ -1,10 +1,11 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class CommandReader : MonoBehaviour
 {
     [SerializeField] Writer writer;
-
+    [SerializeField] ReadCommandExecutor readExecutor;
     public void Read(string command)
     {
         if(string.Compare(command, "help") == 0)
@@ -17,8 +18,33 @@ public class CommandReader : MonoBehaviour
             StartCoroutine(FunFunction());
             return;
         }
-        //if not a coomand
-        StartCoroutine(UnknownCommandReaderReaction());
+        string[] splittedCommand = command.Split(new char[]{' '}, System.StringSplitOptions.RemoveEmptyEntries);
+        if(splittedCommand.Length == 0)
+        {
+            StartCoroutine(UnknownCommandReaderReaction()); 
+            return;
+        }
+        if(splittedCommand[0].CompareTo("read") == 0)
+        {
+            if(splittedCommand.Length != 2)
+            {
+                InvalidArgumentsNumberReaderReaction("read", 1);
+                return;
+            }
+            if(int.TryParse(splittedCommand[1], out int result))
+            {
+                readExecutor.Read(result);
+                return;
+            }
+            else
+            {
+                InvalidArgumentsReaderReaction("read", "integer");
+                return;
+            }
+
+        }  
+
+        StartCoroutine(UnknownCommandReaderReaction()); 
     }
 
     IEnumerator FunFunction()
@@ -28,7 +54,6 @@ public class CommandReader : MonoBehaviour
         yield return new WaitUntil (() => writer.IsWriterReady);
         writer.PrintMomentumMessage("\n\n<color=#ff00ffff>Personal terminal nfPMx16 OS root</color>\n", TerminalManager.TerminalState.input);
     }
-
     IEnumerator PrintHelp()
     {
         writer.PrintMessage("\nHELP shows the command list\n", 
@@ -46,6 +71,20 @@ public class CommandReader : MonoBehaviour
     IEnumerator UnknownCommandReaderReaction(string command)
     {
         writer.PrintMessage('\n' + command + "is unknown command. Use \"help\" to see accessible command list.", TerminalManager.TerminalState.performing);
+        yield return new WaitUntil (() => writer.IsWriterReady);
+        writer.PrintMomentumMessage("\n\n<color=#ff00ffff>Personal terminal nfPMx16 OS root</color>\n", TerminalManager.TerminalState.input);
+    }
+    IEnumerator InvalidArgumentsNumberReaderReaction(string command, int arguments)
+    {
+        writer.PrintMessage("\nThere is too much or not enough arguments, command\n" + command + "\n takes up to " + arguments + " arguments.", 
+        TerminalManager.TerminalState.performing, Writer.PrintSpeed.fast);
+        yield return new WaitUntil (() => writer.IsWriterReady);
+        writer.PrintMomentumMessage("\n\n<color=#ff00ffff>Personal terminal nfPMx16 OS root</color>\n", TerminalManager.TerminalState.input);
+    }
+    IEnumerator InvalidArgumentsReaderReaction(string command, string argumentType)
+    {
+        writer.PrintMessage("\nInvalid argument. Command\n" + command + "\n takes only " + argumentType, 
+        TerminalManager.TerminalState.performing, Writer.PrintSpeed.fast);
         yield return new WaitUntil (() => writer.IsWriterReady);
         writer.PrintMomentumMessage("\n\n<color=#ff00ffff>Personal terminal nfPMx16 OS root</color>\n", TerminalManager.TerminalState.input);
     }
---
+++ ServerSabotage-unity/Assets/GameSystems&Objects/GameTerminal/Commands/ReadCommandExecutor.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ReadCommandExecutor : MonoBehaviour
+{
+    public void Read(int index)
+    {
+        
+    }
+}
