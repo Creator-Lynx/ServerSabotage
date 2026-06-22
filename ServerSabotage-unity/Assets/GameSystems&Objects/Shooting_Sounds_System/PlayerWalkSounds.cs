519e89cad566271ed437b992be182cd8c0c08956
CreatorLynx <1_linx_1@mail.ru>
2026-06-22 19:18:06 +0300

    added weapon clanging sound

--- ServerSabotage-unity/Assets/GameSystems&Objects/Shooting_Sounds_System/PlayerWalkSounds.cs
+++ ServerSabotage-unity/Assets/GameSystems&Objects/Shooting_Sounds_System/PlayerWalkSounds.cs
@@ -9,23 +9,33 @@ public class PlayerWalkSounds : MonoBehaviour
     [SerializeField] float rigthStepPitch = 1.1f;
 
     [Header("Clang Sounds Variants")]
-    [SerializeField] AudioSource weaponClang0Source;
-    [SerializeField] AudioClip weaponClang0Clip;
-    //[SerializeField] AudioSource weaponClang1Source;
-    [SerializeField] AudioClip weaponClang1Clip;
-    //[SerializeField] AudioSource weaponClang2Source;
-    [SerializeField] AudioClip weaponClang2Clip;
+    [SerializeField] AudioSource weaponClangSource;
+    [SerializeField] AudioClip[] weaponClangClip = new AudioClip[3];
+    [SerializeField] float minimumClangPitch = 1f;
+    [SerializeField] float maximumClangPitch = 1.05f;
 
 
     public void LeftStep()
     {
         stepSource.pitch = leftStepStepPitch;
         stepSource.PlayOneShot(stepClip);
+        RandomClangPlay(0.15f);
     }
 
     public void RightStep()
     {
         stepSource.pitch = rigthStepPitch;
         stepSource.PlayOneShot(stepClip);
+        RandomClangPlay(0.6f);
+    }
+
+    void RandomClangPlay(float chance)
+    {
+        if(Random.Range(0f, 1.0f) > chance) return;
+
+        int i = Random.Range(0, 3);
+
+        weaponClangSource.pitch = Random.Range(minimumClangPitch, maximumClangPitch);
+        weaponClangSource.PlayOneShot(weaponClangClip[i]);
     }
 }
