c80aa069e3d028e833156eabe9b129f40504fb3d
CreatorLynx <1_linx_1@mail.ru>
2026-06-24 20:28:38 +0300

    procedural weapon animation: get a look current velocity

--- ServerSabotage-unity/Assets/GameSystems&Objects/WeaponImpactSystem/WeaponInertAnimation.cs
+++ ServerSabotage-unity/Assets/GameSystems&Objects/WeaponImpactSystem/WeaponInertAnimation.cs
@@ -1,16 +1,36 @@
+using Unity.Multiplayer.PlayMode;
 using UnityEngine;
 
 public class WeaponInertAnimation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] float smoothing;
+    [SerializeField] Vector2 extremumRotation;
+    [SerializeField] Vector2 extremumShift;
 
-    // Update is called once per frame
+    Vector3 defaultPosition;
+    Vector3 defaultRotation;
+
+    Vector3 cameraCachedRotation;
+    [Header("current camera velocity (test)")]
+    [SerializeField] Vector2 deltaRotation;
     void Update()
     {
-        
+        //part 1: get a camera velocity
+        deltaRotation.x = (cameraTransform.eulerAngles - cameraCachedRotation).y;
+        deltaRotation.y = (cameraTransform.eulerAngles - cameraCachedRotation).x;
+        cameraCachedRotation = cameraTransform.eulerAngles;
+
+        //part 2: shift by velocity
+
+        //part 3?: move to default position
+
+    }
+
+
+    void Start()
+    {
+        defaultPosition = transform.localPosition;
+        defaultRotation = transform.localEulerAngles;
     }
 }
